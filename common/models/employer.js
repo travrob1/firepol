66d15e4513a6d38c6dbe9c6e33e859b607b13c16
Chris David <chris.david@progfinance.com>
2016-04-21 08:03:28 -0700

    remove userProfile subclass and make it a relation to User.

--- common/models/employer.js
+++ common/models/employer.js
@@ -6,7 +6,6 @@ var moment = require('moment-timezone'),
 module.exports = function(Employer) {
     Employer.observe('before save', function(ctx, next) {
         var employer = ctx.data || ctx.instance;
-        console.log(ctx);
         if (! _.includes(moment.tz.names(), employer.TimeZone)) {
             throw new RangeError('invalid timezone'); 
         }
