3f67f4d1c3e65d688bb99846d69243877c7159e2
Chris David <chris.david@progfinance.com>
2016-04-22 21:45:39 -0700

    get integration tests working after reverting bower madness.

--- karma.integration.conf.js
+++ karma.integration.conf.js
@@ -9,7 +9,7 @@ module.exports = function(config) {
       'bower_components/jquery/dist/jquery.js',
       'bower_components/lodash/dist/lodash.js',
       'bower_components/angular/angular.js',
-      'bower_components/angular-resource/angular-resource.js',
+      'bower_components/angular/angular-resource.js',
       'node_modules/mocha/mocha.js',
       'node_modules/chai/chai.js',
       'node_modules/sinon/pkg/sinon.js',
