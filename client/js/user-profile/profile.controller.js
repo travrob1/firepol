b4f31d83f1dbe8656eead34555b6a7921dc46856
Travis Robison <travis.robison@progfinance.com>
2016-04-26 20:49:13 -0600

    fixing casing to be the same as default loopback app

--- client/js/user-profile/profile.controller.js
+++ client/js/user-profile/profile.controller.js
@@ -5,6 +5,7 @@
 angular.module('app').controller('profileCtrl', function($scope, UserProfile){
     UserProfile.getUserProfileDefinition().$promise
      .then(function(data){
+        debugger
         data.userProfileDefinition.splice(2, 1);
          $scope.userProfileDef = data.userProfileDefinition;
          
