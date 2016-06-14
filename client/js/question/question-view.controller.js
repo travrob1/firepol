273585e2789bd715dc7f840da75c5458bca75ce0
Travis Robison <travis.robison@progfinance.com>
2016-06-13 23:05:23 -0600

    working on comments.  adding timeToWords filter for datestamp

--- client/js/question/question-view.controller.js
+++ client/js/question/question-view.controller.js
@@ -8,10 +8,12 @@ angular.module('app')
 function questionView($scope, $q, $stateParams, Question) {
    $q
      .all([
-        Question.findById({ id: $stateParams.id }).$promise
+        Question.findById({ id: $stateParams.id }).$promise,
+        Question.Comments({ id: $stateParams.id }).$promise
      ])
      .then(function(data) {
         $scope.question = data[0];
+        $scope.comments = data[1];
      });
     
 }
