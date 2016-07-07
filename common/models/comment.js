60bc130eac0e967385bb5ce81da33c9c17e07783
Chris David <chris.david@progfinance.com>
2016-07-06 21:50:01 -0700

    move common code from comment to modelHelpers.  WIP in sense that other models can use code in modelHelpers.

--- common/models/comment.js
+++ common/models/comment.js
@@ -1,35 +1,16 @@
+'use strict';
 var loopback = require('loopback');
+var modelHelpers = require('./modelHelpers');
 
 module.exports = function(Comment) {
-    var app;
-    function getAppReference(cb) {
-        function _getAppReference(cb) {
-            return function(err, theApp) {
-                if (err) console.error(err);
-                app = theApp;
-                cb();
-            };
-        }
-        if (! app) {
-            Comment.getApp(_getAppReference(cb));
-        } else {
-            cb();
-        }
-    }
     Comment.observe('before save', function(ctx, next) {
-        getAppReference(run);
-        function run() {
+        function run(app) {
             var accessToken = loopback.getCurrentContext().active.http.req.accessToken,
                 dataOrInstance = ctx.instance || ctx.data;
             if (!accessToken) {
                 next(new Error('must be logged in to leave comment'));
             } else {
-                
-                app.models.Question.findById(dataOrInstance.questionId, setTimestamp)
-                function setTimestamp(err, question) {
-                    question.updateAttributes({modified: Date.now()});
-                }
-
+                modelHelpers.timestamp(app.models.Question, dataOrInstance.questionId);
                 dataOrInstance.ownerId = accessToken.userId;
                 app.models.FirepolUser.findById(accessToken.userId, function(err, usr){
                     if(usr){
@@ -40,5 +21,6 @@ module.exports = function(Comment) {
             }
             dataOrInstance.time = Date.now();
         }
+        modelHelpers.getAppReference(Comment).then(run);
      });
 };
---
+++ common/models/modelHelpers.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var $q = require('bluebird');
+
+var app;
+function getAppReference(model) {
+    return new $q( function(resolve, reject) {
+        if (! app) {
+            model.getApp(function(err, theApp) {
+                if (err) { reject(err); }
+                else { resolve(theApp); }
+            });
+        } else {
+            resolve(app);
+        }
+    });
+}
+
+function timestamp(model, id, attribute) {
+    // if we ever need it, this should return a promise that resolves when update is complete
+    if (!attribute) { attribute='modified';}
+    var update = {};
+    update[attribute] = Date.now();
+    model.findById(id, setTimestamp);
+    function setTimestamp(err, modelInstance) {
+        modelInstance.updateAttributes(update);
+    }
+}
+module.exports = {
+    getAppReference: getAppReference,
+    timestamp: timestamp
+};
