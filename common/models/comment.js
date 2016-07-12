90445bbd96308676d7d0114142c9ce5a3cab7455
Chris David <chris.david@progfinance.com>
2016-07-11 21:46:51 -0700

    attempt to fix comment.

--- common/models/comment.js
+++ common/models/comment.js
@@ -8,14 +8,15 @@ module.exports = function(Comment) {
             var currentContext = loopback.getCurrentContext(),
                 accessToken = currentContext && currentContext.active.http.req.accessToken,
                 dataOrInstance = ctx.instance || ctx.data;
-            if (! currentContext) {
+            if (! ctx.req) {
                 // assuming we are coming in from db-seed or other non HTTP client.
-            } else if (!accessToken) {
+                next();
+            } else if (!ctx.req.accessToken) {
                 next(new Error('must be logged in to leave comment'));
             } else {
                 modelHelpers.timestamp(app.models.Question, dataOrInstance.questionId);
-                dataOrInstance.ownerId = accessToken.userId;
-                app.models.FirepolUser.findById(accessToken.userId, function(err, usr){
+                dataOrInstance.ownerId = ctx.req.accessToken.userId;
+                app.models.FirepolUser.findById(ctx.req.accessToken.userId, function(err, usr){
                     if(usr){
                         dataOrInstance.name = usr.username;
                     }
