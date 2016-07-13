eff57276ddf88f7d9de8c1ad89691a86a7b8a7b6
Someone on deployment server <deploy@firepol-qa.wherever>
2016-07-13 00:57:26 -0400

    ctx problem solved, but only serves one request then dies.

--- common/models/comment.js
+++ common/models/comment.js
@@ -1,5 +1,4 @@
 'use strict';
-var loopback = require('loopback');
 var modelHelpers = require('./modelHelpers');
 
 module.exports = function(Comment) {
--- server/boot/context.js
+++ server/boot/context.js
@@ -1,12 +1,24 @@
+
+
 function setupContext(app) {
-    // var methods = app.remotes().methods();
-    // for (var i = 0; i < methods.length; i++) {
-    //     console.log(methods[i].stringName, methods[i].name);
-    // }
+    var methods = app.remotes().methods();
+    for (var i = 0; i < methods.length; i++) {
+         console.log(methods[i].stringName, methods[i].name);
+    }
+
+    var methodsWithContext = [];
 
     function inject(ctx, next) {
-        console.log('in inject');
+        console.log('in inject', ctx.method.accepts);
         var options = hasOptions(ctx.method.accepts) && (ctx.args.options || {});
+	if (methodsWithContext.indexOf(ctx.method.stringName) > -1 ) {
+            ctx.method.accepts.push({
+                arg: 'options',
+                type: 'object',
+                injectCtx: true
+            });
+	    options = ctx.args.options || {};
+	}
         if(options) {
             console.log('in inject with options');
             options.accessToken = ctx.req.accessToken;
@@ -30,6 +42,8 @@ function setupContext(app) {
         if(!hasOptions(method.accepts) && method.sharedClass.ctor.definition.settings.accessTokenEnableCtxOption) {
             console.log('adding options to', method.stringName);
             remotes.before(method.stringName, inject);
+	    methodsWithContext.push(method.stringName);
+	    method.foo = 'bar';
             method.accepts.push({
                 arg: 'options',
                 type: 'object',
