6e19ad6e17393acb4e9b30633a787a332f4984ac
Chris David <chris.david@progfinance.com>
2016-07-13 10:38:00 -0700

    context.js is in good shape hopefully.

--- server/boot/context.js
+++ server/boot/context.js
@@ -1,56 +1,34 @@
+function setupContextOptions(app) {
+    var methodsWithContext = [],
+        ctxOption = {
+                arg: 'options',
+                type: 'object'
+            };
 
-
-function setupContext(app) {
     var methods = app.remotes().methods();
     for (var i = 0; i < methods.length; i++) {
          console.log(methods[i].stringName, methods[i].name);
     }
 
-    var methodsWithContext = [];
-
     function inject(ctx, next) {
         console.log('in inject', ctx.method.accepts);
-        var options = hasOptions(ctx.method.accepts) && (ctx.args.options || {});
-	if (methodsWithContext.indexOf(ctx.method.stringName) > -1 ) {
-            ctx.method.accepts.push({
-                arg: 'options',
-                type: 'object',
-                injectCtx: true
-            });
-	    options = ctx.args.options || {};
-	}
-        if(options) {
-            console.log('in inject with options');
-            options.accessToken = ctx.req.accessToken;
-            ctx.args.options = options;
+        var options = ctx.args.options || {};
+        if (ctx.method.accepts.indexOf(ctxOption) === -1) {
+            ctx.method.accepts.push(ctxOption);
         }
+        options.accessToken = ctx.req.accessToken;
+        ctx.args.options = options;
         next();
-    }
-
-    function hasOptions(accepts) {
-        for (var i = 0; i < accepts.length; i++) {
-            var argDesc = accepts[i];
-            if (argDesc.arg === 'options' && argDesc.injectCtx) {
-                return true;
-            }
-        }
-        return false;
     }
 
     var remotes = app.remotes();
     remotes.methods().forEach(function(method) {
-        if(!hasOptions(method.accepts) && method.sharedClass.ctor.definition.settings.accessTokenEnableCtxOption) {
+        if(method.sharedClass.ctor.definition.settings.accessTokenEnableCtxOption) {
             console.log('adding options to', method.stringName);
             remotes.before(method.stringName, inject);
-	    methodsWithContext.push(method.stringName);
-	    method.foo = 'bar';
-            method.accepts.push({
-                arg: 'options',
-                type: 'object',
-                injectCtx: true
-            });
+            methodsWithContext.push(method.stringName);
         }
     });
 }
 
-module.exports = setupContext;
+module.exports = setupContextOptions;
